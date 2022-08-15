692cd75793ffa4d6be3bfe32731255147f355c9e
yakov kuzin <kuzin_js@mail.ru>
2022-08-15 19:59:16 +0300

    Homework19 added. Test version

--- homework19/Program.cs
+++ homework19/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите 5-ти значное число");
+int numb = Convert.ToInt32(Console.ReadLine());
+numb = Math.Abs(numb);
+
+if (numb < 100000 && numb >= 10000)
+{
+    int a = numb / 10000;
+    int b = numb % 10;
+    int c = (numb / 1000) % 10;
+    int d = (numb % 100) / 10;
+
+    if (a == b)
+    {
+        if (c == d)
+        {
+            Console.WriteLine("Это числовой палиндром");
+        }
+    }
+    else
+    {
+        Console.WriteLine("Это не числовой палиндром");
+    }
+}
+else
+{
+    Console.WriteLine("Введите число согласно условиям задачи");
+}
