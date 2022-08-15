11ad2c13d900a6eb3e8627df123d52edf3de758c
yakov kuzin <kuzin_js@mail.ru>
2022-08-15 23:15:18 +0300

    Homework23 added

---
+++ homework23/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Programm
+{
+    static void Main(string[] cube_numb)
+    {
+        Console.WriteLine("Введите число ");
+        int numb = Convert.ToInt32(Console.ReadLine());
+
+        double cube = 1;
+
+        for (int i = 1; i <= numb; i++ )
+        {
+            cube = Math.Pow(i, 3);
+            Console.Write(cube + ", ");
+        }
+        Console.Write("таблица кубов чисел от 1 до введённого Вами числа.");
+    }
+}
