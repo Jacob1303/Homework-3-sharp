2b63b2eb2332e67ab458fb24e464492c4ae2b240
yakov kuzin <kuzin_js@mail.ru>
2022-08-15 20:17:25 +0300

    Homework19 added

--- homework19/Program.cs
+++ homework19/Program.cs
@@ -1,27 +1,35 @@
-﻿Console.WriteLine("Введите 5-ти значное число");
-int numb = Convert.ToInt32(Console.ReadLine());
-numb = Math.Abs(numb);
+﻿using System;
 
-if (numb < 100000 && numb >= 10000)
+class Programm
 {
-    int a = numb / 10000;
-    int b = numb % 10;
-    int c = (numb / 1000) % 10;
-    int d = (numb % 100) / 10;
-
-    if (a == b)
+    static void Main(string[] third_num)
     {
-        if (c == d)
+        Console.WriteLine("Введите 5-ти значное число");
+        int numb = Convert.ToInt32(Console.ReadLine());
+        numb = Math.Abs(numb);
+
+        if (numb < 100000 && numb >= 10000)
         {
-            Console.WriteLine("Это числовой палиндром");
+            int a = numb / 10000;
+            int b = numb % 10;
+            int c = (numb / 1000) % 10;
+            int d = (numb % 100) / 10;
+
+            if (a == b)
+            {
+                if (c == d)
+                {
+                    Console.WriteLine("Это числовой палиндром");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Это не числовой палиндром");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Введите число согласно условиям задачи");
         }
     }
-    else
-    {
-        Console.WriteLine("Это не числовой палиндром");
-    }
-}
-else
-{
-    Console.WriteLine("Введите число согласно условиям задачи");
 }
